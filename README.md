2552e201f18333ecfb6a7679bad9c51541d1a9f7
hatembellaaj <hatem.bellaaj@redcad.org>
2022-05-06 14:38:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-c7jukg-ky2fez
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-c7jukg-ky2fez)
---
+++ src/app/menu-position-example.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Menu positioning
+ */
+@Component({
+  selector: 'menu-position-example',
+  templateUrl: 'menu-position-example.html',
+})
+export class MenuPositionExample {}
+
+
+/**  Copyright 2022 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
